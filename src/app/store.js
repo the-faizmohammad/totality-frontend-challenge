89462e02e62160f0f1e611f6d0ddf58601b47815
user-faiz <email2faizm@gmail.com>
2024-08-31 15:53:27 +0530

    Add listingsReducer to Redux store configuration

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import listingsReducer from '../features/listings/listingsSlice';
+
 export const store = configureStore({
   reducer: {
+    listings: listingsReducer,
   },
 });
